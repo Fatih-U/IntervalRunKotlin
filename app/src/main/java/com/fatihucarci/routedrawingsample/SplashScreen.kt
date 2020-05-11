f5681bd6ead7c38f9df6485e201ef00a909637e9
fatih.ucarci <fatih.ucarci@gmail.com>
2020-05-11 11:17:59 +0400

    Unnecessary todo lines deleted

---
+++ app/src/main/java/com/fatihucarci/routedrawingsample/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.fatihucarci.routedrawingsample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        iv_splash_logo.alpha = 0f
+        iv_splash_logo.animate().setDuration(1500).alpha(1f).withEndAction {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
